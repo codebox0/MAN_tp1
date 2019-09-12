8fc5fd91a0e079236645474e06202a1e3869f7d4
Codebox <codebox@localhost.localdomain>
2019-09-12 14:18:40 +0200

    Test Integration Jenkins 3

--- src/main/java/tp1/JXBusyLabelTest.java
+++ src/main/java/tp1/JXBusyLabelTest.java
@@ -77,6 +77,7 @@ public class JXBusyLabelTest extends JFrame implements ActionListener {
 	public void actionPerformedTest(ActionEvent e) {
 		System.out.println("TEST voir");
 		System.out.println("TEST voir2 ");
+		System.out.println("TEST voir3 ");
 	}
 
 	/**
